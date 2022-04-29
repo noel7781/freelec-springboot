f9601d8b7d13913ab97c0ad8f6ad55805fedfd38
noel7781 <noel7781@naver.com>
2022-04-29 20:59:41 +0900

    chapter 10

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -6,7 +6,7 @@ source ${ABSDIR}/profile.sh
 
 IDLE_PORT=$(find_idle_port)
 
-echo "> $IDLE_PORT 에서 구동 중인 애플리케이션 pid 확인"
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
@@ -15,5 +15,5 @@ then
 else
   echo "> kill -15 $IDLE_PID"
   kill -15 ${IDLE_PID}
-  sleep 10
+  sleep 5
 fi
